1418543753cfeaff2979cd340686143d4706bd19
JAG <jagrasso42@gmail.com>
2022-12-27 13:55:54 -0500

    add categories array to map category components

--- src/App.js
+++ src/App.js
@@ -1,48 +1,33 @@
 const App = () => {
+  const categories = [
+    {
+      title: 'Hats',
+    },
+    {
+      title: 'Jackets',
+    },
+    {
+      title: 'Sneakers',
+    },
+    {
+      title: 'Womens',
+    },
+    {
+      title: 'Mens',
+    },
+  ];
+
   return (
     <div className="categories-container">
-      <div className="category-container">
-        {/* <img /> */}
-        <div className="category-body-container">
-          <h2>Hats</h2>
-          <p>Shop Now</p>
+      {categories.map(({ title }, idx) => (
+        <div className="category-container">
+          <div className="background-image" />
+          <div className="category-body-container">
+            <h2>{title}</h2>
+            <p>Shop Now</p>
+          </div>
         </div>
-      </div>
-      <div className="category-container">
-        {/* <img /> */}
-        <div className="category-body-container">
-          <h2>Jackets</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-      <div className="category-container">
-        {/* <img /> */}
-        <div className="category-body-container">
-          <h2>Sneakers</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-      <div className="category-container">
-        {/* <img /> */}
-        <div className="category-body-container">
-          <h2>Womens</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-      <div className="category-container">
-        {/* <img /> */}
-        <div className="category-body-container">
-          <h2>Men</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-      <div className="category-container">
-        {/* <img /> */}
-        <div className="category-body-container">
-          <h2>Hats</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
+      ))}
     </div>
   );
 };
