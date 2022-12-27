6acaf6a93e1a28a11a5ada7c7abe66eb575cfe6d
JAG <jagrasso42@gmail.com>
2022-12-27 14:14:04 -0500

    add images to categories array and set background image

--- src/App.js
+++ src/App.js
@@ -1,27 +1,39 @@
+import './categories.styles.scss';
+
 const App = () => {
   const categories = [
     {
-      title: 'Hats',
+      title: 'hats',
+      imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
     },
     {
-      title: 'Jackets',
+      title: 'jackets',
+      imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
     },
     {
-      title: 'Sneakers',
+      title: 'sneakers',
+      imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
     },
     {
-      title: 'Womens',
+      title: 'womens',
+      imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
     },
     {
-      title: 'Mens',
+      title: 'mens',
+      imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
     },
   ];
 
   return (
     <div className="categories-container">
-      {categories.map(({ title }, idx) => (
-        <div className="category-container">
-          <div className="background-image" />
+      {categories.map(({ title, imageUrl }, idx) => (
+        <div key={idx} className="category-container">
+          <div
+            className="background-image"
+            style={{
+              backgroundImage: `url(${imageUrl})`,
+            }}
+          />
           <div className="category-body-container">
             <h2>{title}</h2>
             <p>Shop Now</p>
