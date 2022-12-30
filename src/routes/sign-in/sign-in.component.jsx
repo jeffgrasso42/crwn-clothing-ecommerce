39db3957c28f57843e25071a53f70c4b63f0824c
JAG <jagrasso42@gmail.com>
2022-12-29 19:00:32 -0500

    remove redirect sign in method

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,17 +1,6 @@
-import { useEffect } from 'react';
-import { getRedirectResult } from 'firebase/auth';
-import { auth, signInWithGooglePopup, signInWithGoogleRedirect, createUserDocumentFromAuth } from '../../utils/firebase/firebase.utils'
+import { signInWithGooglePopup, signInWithGoogleRedirect, createUserDocumentFromAuth } from '../../utils/firebase/firebase.utils'
 
 const SignIn = () => {
-  useEffect(() => {
-    const getResponse = async() => {
-      const response = await getRedirectResult(auth);
-      if (response) {
-        const userDocRef = await createUserDocumentFromAuth(response.user);
-      }
-    }
-    getResponse();
-  }, [])
 
   const logGoogleUser = async () => {
     const { user } = await signInWithGooglePopup();
@@ -22,7 +11,6 @@ const SignIn = () => {
     <div>
       <h1>Sign In Page</h1>
       <button onClick={logGoogleUser}>Sign in with Google Popup</button>
-      <button onClick={signInWithGoogleRedirect}>Sign in with Google Redirect</button>
     </div>
   )
 }
