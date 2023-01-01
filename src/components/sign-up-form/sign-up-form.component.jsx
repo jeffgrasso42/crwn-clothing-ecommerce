76f4bb154553c8203d55ab89fcc607684cf7546c
JAG <jagrasso42@gmail.com>
2023-01-01 16:55:58 -0500

    add automatic sign in when user successfully sign up. add conditionally rendered sign out button to navigation component

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -1,9 +1,11 @@
-import { useState } from "react"
+import { useState, useContext } from "react"
 
 import FormInput from "../form-input/form-input.component"
 import Button from "../button/button.component"
 
 import { createAuthUserWithEmailAndPassword, createUserDocumentFromAuth } from '../../utils/firebase/firebase.utils'
+
+import { UserContext } from "../../context/user.context"
 
 import './sign-up-form.styles.scss'
 
@@ -17,6 +19,8 @@ const defaultFormFields = {
 const SignUpForm = () => {
   const [formFields, setFormFields] = useState(defaultFormFields);
   const { displayName, email, password, confirmPassword } = formFields;
+  const { setCurrentUser } = useContext(UserContext);
+
 
   const resetFormFields = () => {
     setFormFields(defaultFormFields);
@@ -32,7 +36,9 @@ const SignUpForm = () => {
     try {
       const { user } = await createAuthUserWithEmailAndPassword(email, password);
 
-      await createUserDocumentFromAuth(user, { displayName })
+      setCurrentUser(user);
+      
+      await createUserDocumentFromAuth(user, { displayName });
       resetFormFields();
     } catch (err) {
       if (err.code === 'auth/email-already-in-use') {
