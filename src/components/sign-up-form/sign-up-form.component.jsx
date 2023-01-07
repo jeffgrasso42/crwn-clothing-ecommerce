8c7ca5b73e94a8fd87973cce6c5a6e6382fc1200
JAG <jagrasso42@gmail.com>
2023-01-06 19:15:36 -0500

    replace SASS styles with styled components

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -5,7 +5,7 @@ import Button from "../button/button.component"
 
 import { createAuthUserWithEmailAndPassword, createUserDocumentFromAuth } from '../../utils/firebase/firebase.utils'
 
-import './sign-up-form.styles.scss'
+import { SignUpContainer } from './sign-up-form.styles.jsx'
 
 const defaultFormFields = {
   displayName: '',
@@ -51,7 +51,7 @@ const SignUpForm = () => {
   }
 
   return (
-    <div className='sign-up-container'>
+    <SignUpContainer>
       <h2>Don't have an account?</h2>
       <span>Sign Up with your email and password</span>
       <form onSubmit={handleSubmit}>
@@ -96,7 +96,7 @@ const SignUpForm = () => {
           }} />
         <Button type='submit'>Sign Up</Button>
       </form>
-    </div>
+    </SignUpContainer>
   )
 }
 
