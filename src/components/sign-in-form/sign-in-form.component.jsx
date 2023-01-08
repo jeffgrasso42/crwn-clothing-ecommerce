69010d2c66b74a32a67e5c0d1cd7d57d6279498f
JAG <jagrasso42@gmail.com>
2023-01-08 16:42:28 -0500

    replace SASS styles with styled components

--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -5,7 +5,7 @@ import Button, { BUTTON_TYPE_CLASSES } from "../button/button.component"
 
 import { signInWithGooglePopup, signInAuthUserWithEmailAndPassword } from '../../utils/firebase/firebase.utils'
 
-import './sign-in-form.styles.scss'
+import { SignUpContainer, ButtonsContainer } from './sign-in-form.styles.jsx'
 
 const defaultFormFields = {
   email: '',
@@ -29,7 +29,7 @@ const SignInForm = () => {
     e.preventDefault();
 
     try {
-      const { user } = await signInAuthUserWithEmailAndPassword(email, password);
+      await signInAuthUserWithEmailAndPassword(email, password);
 
       resetFormFields();
     } catch (err) {
@@ -53,7 +53,7 @@ const SignInForm = () => {
   }
 
   return (
-    <div className='sign-up-container'>
+    <SignUpContainer>
       <h2>Already have an account?</h2>
       <span>Sign in with your email and password</span>
       <form onSubmit={handleSubmit}>      
@@ -77,7 +77,7 @@ const SignInForm = () => {
             value: password
           }} />
 
-        <div className='buttons-container'>
+        <ButtonsContainer>
           <Button type='submit'>Sign In</Button>
           <Button
             type='button'
@@ -86,9 +86,9 @@ const SignInForm = () => {
           >
             Sign In With Google
           </Button>
-        </div>
+        </ButtonsContainer>
       </form>
-    </div>
+    </SignUpContainer>
   )
 }
 
